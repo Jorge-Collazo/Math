4de454132adf7bc313aa477bee006383b50eab86
Jorge Collazo <jcollazo151@west-mec.org>
2020-01-27 00:01:46 -0700

    Math is still math

--- math.js
+++ math.js
@@ -136,7 +136,139 @@ function leastToGreat(){
 
     o6 = one
 
-    output.innerHTML = o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5 + ", " + o6 + ", "; 
+    output.innerHTML = o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5 + ", " + o6; 
+    return true;
+}
+
+function greatToLeast(){
+    var one = parseFloat(num1.value);
+    var two = parseFloat(num2.value);
+    var three = parseFloat(num3.value);
+    var four = parseFloat(num4.value);
+    var five = parseFloat(num5.value);
+    var six = parseFloat(num6.value);
+    output.innerHTML = "";
+
+    var o1 = 0;
+    var o2 = 0;
+    var o3 = 0;
+    var o4 = 0;
+    var o5 = 0;
+    var o6 = 0;
+
+    o1 = Math.min(one, two, three, four, five, six);
+    
+    if(o1 === one){
+        one = two;
+        two = three;
+        three = four;
+        four = five;
+        five = six;
+    }else if(o1 === two){
+        one = one;
+        two = three;
+        three = four;
+        four = five;
+        five = six;
+    }else if(o1 === three){
+        one = one;
+        two = two;
+        three = four;
+        four = five;
+        five = six;
+    }else if(o1 === four){
+        one = one;
+        two = two;
+        three = three;
+        four = five;
+        five = six;
+    }else if(o1 === five){
+        one = one;
+        two = two;
+        three = three;
+        four = four;
+        five = six;
+    }else if(o1 === six){
+        one = one;
+        two = two;
+        three = three;
+        four = four;
+        five = five;
+    }
+    
+    o2 = Math.min(one, two, three, four, five);
+
+    if(o2 === one){
+        one = two;
+        two = three;
+        three = four;
+        four = five;
+    }else if(o2 === two){
+        one = one;
+        two = three;
+        three = four;
+        four = five;
+    }else if(o2 === three){
+        one = one;
+        two = two;
+        three = four;
+        four = five;
+    }else if(o2 === four){
+        one = one;
+        two = two;
+        three = three;
+        four = five;
+    }else if(o2 === five){
+        one = one;
+        two = two;
+        three = three;
+        four = four;
+    }
+
+    o3 = Math.min(one, two, three, four);
+
+    if(o3 === one){
+        one = two;
+        two = three;
+        three = four;
+    }else if(o3 === two){
+        one = one;
+        two = three;
+        three = four;
+    }else if(o3 === three){
+        one = one;
+        two = two;
+        three = four;
+    }else if(o3 === four){
+        one = one;
+        two = two;
+        three = three;
+    }
+
+    o4 = Math.min(one, two, three);
+
+    if(o4 === one){
+        one = two;
+        two = three;
+    }else if(o4 === two){
+        one = one;
+        two = three;
+    }else if(o4 === three){
+        one = one;
+        two = two;
+    }
+
+    o5 = Math.min(one, two);
+
+    if(o5 === one){
+        one = two;
+    }else if(o5 === two){
+        one = one;
+    }
+
+    o6 = one
+
+    output.innerHTML = o6 + ", " + o5 + ", " + o4 + ", " + o3 + ", " + o2 + ", " + o1; 
     return true;
 }
 
